68e424ff55984b089aaa474d590445cea5e917a8
Sullivan008 <kozakpeter0424@gmail.com>
2019-08-15 17:07:14 +0200

    INI Application Demo Project Initialization

---
+++ 03_INIApplicationDemo/INIApplicationDemo/ControlForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace INIApplicationDemo
+{
+	partial class ControlForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.readValueLabel = new System.Windows.Forms.Label();
+            this.saveSettingsCheckBox = new System.Windows.Forms.CheckBox();
+            this.readValueResultLabel = new System.Windows.Forms.Label();
+            this.userNameResultLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.readValueTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(16, 29);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.userNameLabel.TabIndex = 0;
+            this.userNameLabel.Text = "User name:";
+            // 
+            // readValueLabel
+            // 
+            this.readValueLabel.AutoSize = true;
+            this.readValueLabel.Location = new System.Drawing.Point(16, 60);
+            this.readValueLabel.Name = "readValueLabel";
+            this.readValueLabel.Size = new System.Drawing.Size(65, 13);
+            this.readValueLabel.TabIndex = 1;
+            this.readValueLabel.Text = "Read value:";
+            // 
+            // saveSettingsCheckBox
+            // 
+            this.saveSettingsCheckBox.AutoSize = true;
+            this.saveSettingsCheckBox.Location = new System.Drawing.Point(19, 113);
+            this.saveSettingsCheckBox.Name = "saveSettingsCheckBox";
+            this.saveSettingsCheckBox.Size = new System.Drawing.Size(90, 17);
+            this.saveSettingsCheckBox.TabIndex = 5;
+            this.saveSettingsCheckBox.Text = "Save settings";
+            this.saveSettingsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // readValueResultLabel
+            // 
+            this.readValueResultLabel.AutoSize = true;
+            this.readValueResultLabel.Location = new System.Drawing.Point(16, 258);
+            this.readValueResultLabel.Name = "readValueResultLabel";
+            this.readValueResultLabel.Size = new System.Drawing.Size(65, 13);
+            this.readValueResultLabel.TabIndex = 4;
+            this.readValueResultLabel.Text = "Read value:";
+            // 
+            // userNameResultLabel
+            // 
+            this.userNameResultLabel.AutoSize = true;
+            this.userNameResultLabel.Location = new System.Drawing.Point(16, 227);
+            this.userNameResultLabel.Name = "userNameResultLabel";
+            this.userNameResultLabel.Size = new System.Drawing.Size(61, 13);
+            this.userNameResultLabel.TabIndex = 3;
+            this.userNameResultLabel.Text = "User name:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(138, 166);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(112, 26);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(209, 20);
+            this.userNameTextBox.TabIndex = 7;
+            // 
+            // readValueTextBox
+            // 
+            this.readValueTextBox.Location = new System.Drawing.Point(112, 57);
+            this.readValueTextBox.Name = "readValueTextBox";
+            this.readValueTextBox.Size = new System.Drawing.Size(209, 20);
+            this.readValueTextBox.TabIndex = 8;
+            // 
+            // ControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 316);
+            this.Controls.Add(this.readValueTextBox);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.saveSettingsCheckBox);
+            this.Controls.Add(this.readValueResultLabel);
+            this.Controls.Add(this.userNameResultLabel);
+            this.Controls.Add(this.readValueLabel);
+            this.Controls.Add(this.userNameLabel);
+            this.Name = "ControlForm";
+            this.Text = "INI Application Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ControlForm_FormClosing);
+            this.Load += new System.EventHandler(this.ControlForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label userNameLabel;
+		private System.Windows.Forms.Label readValueLabel;
+		private System.Windows.Forms.CheckBox saveSettingsCheckBox;
+		private System.Windows.Forms.Label readValueResultLabel;
+		private System.Windows.Forms.Label userNameResultLabel;
+		private System.Windows.Forms.Button saveButton;
+		private System.Windows.Forms.TextBox userNameTextBox;
+		private System.Windows.Forms.TextBox readValueTextBox;
+	}
+}
+
---
+++ 03_INIApplicationDemo/INIApplicationDemo/ControlForm.cs
@@ -0,0 +1,115 @@
+﻿using INIApplicationDemo.Properties;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace INIApplicationDemo
+{
+    public partial class ControlForm : Form
+	{
+        private bool dataIsSaved;
+
+        /// <summary>
+        ///     Konstruktor.
+        /// </summary>
+		public ControlForm()
+		{
+			InitializeComponent();
+
+            dataIsSaved = false;
+		}
+
+        #region EVENTS
+        /// <summary>
+        ///     LOAD EVENT - Az esemény akkor fut le, amikor a Form betöltődik. Inizializáljuk a képernyő méretét
+        ///     illetve beolvassuk a program előző állapotát, amelyet bezáráskor mentettünk le.
+        /// </summary>
+        private void ControlForm_Load(object sender, EventArgs e)
+		{
+            SetWindowStateAndSize();
+
+            LoadPreviousProgramState();
+        }
+
+        /// <summary>
+        ///     CLICK EVENT - Az adatok mentését végrehajtó metódus.
+        /// </summary>
+		private void SaveButton_Click(object sender, EventArgs e)
+		{
+			if (saveSettingsCheckBox.Checked)
+			{
+				try
+				{
+                    /// INPUT TextBox értékeinek mentése.
+                    Settings.Default.UserName = userNameTextBox.Text;
+					Settings.Default.ReadValue = int.Parse(readValueTextBox.Text);
+
+                    /// FORM POSITION adatainak mentése.
+                    Settings.Default.PosX = Left;
+					Settings.Default.PosY = Top;
+					Settings.Default.ControlFormHeight = Size.Height;
+					Settings.Default.ControlFormWidth = Size.Width;
+
+					Settings.Default.Save();
+					dataIsSaved = true;
+
+					MessageBox.Show("Data Save Successfully!", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				catch (FormatException)
+				{
+					MessageBox.Show("ERROR - Incorrect values in the \"Scanned Value\" field!", "Failed to save",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+					UseWaitCursor = false;
+				}
+			}
+			else
+			{
+				MessageBox.Show("Please select the \"Save Settings\" checkbox!", "Operation failed!",
+					MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+        /// <summary>
+        ///     FORM CLOSING EVENT - A metódus akkor fut le, amikor a címsorban található Kliépés gombra kattintunk.
+        ///     Megvizsgáljuk, hogy az adatok mentésre kerültek-e már. Ha még nem, akkor felajánljuk a felhasználónak, 
+        ///     hogy elmenthesse.
+        /// </summary>
+		private void ControlForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (saveSettingsCheckBox.Checked && !dataIsSaved)
+			{
+				DialogResult result = MessageBox.Show("The settings have not been saved. Do you want to save it?", "Exit",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+				if (result == DialogResult.Yes)
+				{
+					saveButton.PerformClick();
+				}
+			}
+		}
+        #endregion
+
+        #region PRIVATE HELPER Methods
+        /// <summary>
+        ///     A SETTINGS-ből beolvassuk a program előzőleg elhelyezett pozícióját és méretezését.
+        /// </summary>
+        private void SetWindowStateAndSize()
+        {
+            DesktopLocation = new Point(Settings.Default.PosX, Settings.Default.PosY);
+
+            Size = new Size(Settings.Default.ControlFormWidth,
+                Settings.Default.ControlFormHeight);
+        }
+
+        /// <summary>
+        ///     A SETTINGS-ből beolvassuk a program előző állapotát. (Milyen szövegek voltak eltárolva, stb...)
+        /// </summary>
+        private void LoadPreviousProgramState()
+        {
+            userNameResultLabel.Text += $" {Convert.ToString(Settings.Default.UserName)}";
+            readValueResultLabel.Text += $" {Convert.ToString(Settings.Default.ReadValue)}";
+        }
+        #endregion
+    }
+}
